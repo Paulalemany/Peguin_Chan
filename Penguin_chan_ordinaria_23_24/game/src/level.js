e741f071d9bbc4a8b3b0aa74775ad44f0cbe91e1
Paulalemany <palemany@ucm.es>
2024-06-16 18:53:10 +0200

    Segundo apartado hecho
    Aprendido ha ahacer el contador

--- Penguin_chan_ordinaria_23_24/game/src/level.js
+++ Penguin_chan_ordinaria_23_24/game/src/level.js
@@ -24,6 +24,32 @@ export default class Level extends Phaser.Scene {
         //Colocamos todo lo visual
         this.bg = this.add.image(256, 255, "fondo");    //fondo
         this.mesa = this.add.image(240, 320, "mesa");   //Mesa
-        this.score = this.add.image(400,300, "score");  //Puntuación
+        this.score = this.add.image(400, 300, "score");  //Puntuación
+
+        /* CONTADOR */
+        this.time = 90  //Tiempo que vamos a ir modificando
+        this.contador = this.add.text(230, 30, this.time, { //Texto
+            fontFamily: 'Babelgam',
+            fontSize: 25,
+            stroke: '#000000',
+            strokeThickness: 3,
+            color: '#FFFFFF',
+            align: 'center'
+        })
+
+        //Contador en si
+        //Creamos un intervalo 
+        //Ponemos lo que tiene que hacer al pasar el tiempo
+        //Ponemos cada cuanto tiempo tiene que hacer la acción
+        this.intervaloContador = setInterval(() => {
+            this.time--;
+        }, 1000);
+    }
+
+    update() {
+        if (this.time >= 0) {
+            this.contador.setText(this.time);
+        }
+       
     }
 }
